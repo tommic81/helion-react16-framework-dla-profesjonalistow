74382f0a2d9737ca529f9fb238971f36da27b0ee
tommic81 <tom.michnik@gmail.com>
2026-09-15 11:50:07 +0200

    First application: creating and configuring a project

---
+++ zadania/src/App.js
@@ -0,0 +1,30 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "Adam",
+    };
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Jakub" : "Adam",
+    });
+  };
+  render = () => (
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        Lista zadań użytkownika {this.state.userName}
+      </h4>
+
+      <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+        Zmień
+      </button>
+    </div>
+  );
+}
