47aa0453219113c38e8ff22ecdba67e3a77fe9c2
tommic81 <tom.michnik@gmail.com>
2026-09-15 15:20:56 +0200

    First application: added a functionality of list of tasks

--- zadania/src/App.js
+++ zadania/src/App.js
@@ -8,23 +8,94 @@ export default class App extends Component {
 
     this.state = {
       userName: "Adam",
+
+      todoItems: [
+        { action: "Kupić kwiaty", done: false },
+
+        { action: "Wziąć buty", done: false },
+
+        { action: "Zebrać bilety", done: true },
+
+        { action: "Zadzwonić do Jurka", done: false },
+      ],
+
+      newItemText: "",
     };
   }
 
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === "Adam" ? "Jakub" : "Adam",
-    });
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
   };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(
+        (item) => item.action === this.state.newItemText,
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false },
+        ],
+
+        newItemText: "",
+      });
+    }
+  };
+
+  toggleTodo = (todo) =>
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+        item.action === todo.action ? { ...item, done: !item.done } : item,
+      ),
+    });
+
+  todoTableRows = () =>
+    this.state.todoItems.map((item) => (
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={() => this.toggleTodo(item)}
+          />
+        </td>
+      </tr>
+    ));
+
   render = () => (
     <div>
       <h4 className="bg-primary text-white text-center p-2">
         Lista zadań użytkownika {this.state.userName}
+        (Liczba zadań: {this.state.todoItems.filter((t) => !t.done).length})
       </h4>
 
-      <button className="btn btn-primary m-2" onClick={this.changeStateData}>
-        Zmień
-      </button>
+      <div className="container-fluid">
+        <div className="my-1">
+          <input
+            className="form-control"
+            value={this.state.newItemText}
+            onChange={this.updateNewTextValue}
+          />
+
+          <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>
+            Dodaj
+          </button>
+        </div>
+
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Opis</th>
+              <th>Wykonane</th>
+            </tr>
+          </thead>
+
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
